753a5d9c4562cc725855202ba0c0ee699958b252
Michael Gallahan <gallahanm@gmail.com>
2021-07-19 21:27:05 -0400

    js now calculates the score based on the users answers

--- sandbox.js
+++ sandbox.js
@@ -1,1 +1,16 @@
-console.log('Hello');
+const correctAnswers = ['D','D','C','B'];
+let form = document.querySelector('.form-check');
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    let score = 0;
+    const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value];
+    userAnswers.forEach((ans, i) => {
+        if (ans === correctAnswers[i])
+            score++;
+    });
+
+    let percentCorrect = score/correctAnswers.length * 100;
+    console.log(percentCorrect);
+});
